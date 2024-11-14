6b30f6b274442ea67a35e9c7f6c10d87131a294e
dylan <dylan.gabriele@vtsd.com>
2024-11-14 10:26:32 -0500

    1-2-1 Day 1

---
+++ 1-2-2/step 50.py
@@ -0,0 +1,17 @@
+# initialize variables
+x = 4
+y = 8
+z = 12
+
+# print the variables
+print("x =", x)
+print("y =", y)
+print("z =", z)
+print("\n")
+
+# try some relational and logic operations
+print("x > y and z > y evaluates to", x > y and z > y)
+print("x > y or z > y evaluates to", x > y or z > y)
+print("x < y and y < z evaluates to", x < y and y < z)
+print("not (x > y) evaluates to", not (x > y))
+print("not (x < y) evaluates to", not (x < y))
