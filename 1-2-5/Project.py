912e9836766081cf75d74d2d6ea3065e6006518c
dylan <dylan.gabriele@vtsd.com>
2024-12-12 13:09:58 -0500

    pacman game

--- 1-2-5/Project.py
+++ 1-2-5/Project.py
@@ -22,7 +22,8 @@ wn.addshape(indigo_alien)
 wn.addshape(human)
 
 #Alien image list
-aliens = ["blue_alien", "purple_alien", "red_alien", "indigo_alien"]
+aliens = [blue_alien,purple_alien,red_alien,indigo_alien]
+
 
 #Background
 wn.bgpic(background)
@@ -30,23 +31,66 @@ wn.bgpic(background)
 #Turtles
 #Player turtle
 player = trtl.Turtle()
+player.turtlesize()
 player.shape(human)
 player.penup()
 player.forward(10)
 
-#Alien vertical turtle
-alien_vert = trtl.Turtle()
-alien_vert.shape(alien[rand.randint(0, (len(aliens))])
-alien_vert.penup()
-player.forward(20)
+#Randomizer
 
-#Alien horizontal turtle
-alien_hor = trtl.Turtle()
-alien_hor.shape(rand.randchoice(aliens))
-alien_hor.penup()
-alien_hor.forward(30)
+#Alien turtle
+alien = trtl.Turtle()
+alien.shape(blue_alien)
+alien.penup()
 
+#Movement functions
+def w_up():
+    player.setheading(90)
+    player.forward(50)
+def s_down():
+    player.setheading(90)
+    player.backward(50)
+
+def a_left():
+    player.setheading(180)
+    player.forward(50)
+
+def d_right():
+    player.setheading(180)
+    player.backward(50)
+
+#Alien
+def i_up():
+    alien.setheading(90)
+    alien.forward(70)
+
+def k_down():
+    alien.setheading(90)
+    alien.backward(70)
+
+def j_left():
+    alien.setheading(180)
+    alien.forward(70)
+
+
+def l_right():
+    alien.setheading(180)
+    alien.backward(70)
+
+
+#Player movement
+wn.onkey(w_up, "w")
+wn.onkey(s_down, "s")
+wn.onkey(a_left, "a")
+wn.onkey(d_right, "d")
+
+#Alien Movement
+wn.onkey(i_up, "i")
+wn.onkey(k_down, "k")
+wn.onkey(j_left, "j")
+wn.onkey(l_right, "l")
 
 
 #Turtle screen
+wn.listen()
 wn.mainloop()
