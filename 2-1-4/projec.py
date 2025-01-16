3939b9d2ffbf12c9854ea73b988e1229befe595b
dylan <dylan.gabriele@vtsd.com>
2025-01-16 12:53:11 -0500

    tk practice

---
+++ 2-1-4/projec.py
@@ -0,0 +1,34 @@
+#--Setup--
+#---Import Statements---
+import turtle
+import turtle as trtl
+import random as random
+
+#--Turtle Screen Initialization--
+wn = trtl.screen()
+
+#--Board--
+board = [["" for _ in range(3)] for _ in range(3)]
+player_1 = "X"
+tile_size = 250
+
+#--Drawing Board--
+def draw_board(board):
+    turtle_speed(10)
+    turtle.penup()
+    turtle.goto(-300, 100)
+    turtle.pendown()
+    turtle.goto(300, 100)
+    turtle.penup()
+    turtle.goto(-300, -100)
+    turtle.pendown()
+    turtle.goto(300, -100)
+    turtle.penup()
+    turtle.goto(-100, 300)
+    turtle.pendown()
+    turtle.goto(-100, -300)
+    turtle.penup()
+    turtle.goto(100, 300)
+    turtle.pendown()
+    turtle.goto(100, -300)
+    turtle.penup()
