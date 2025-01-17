35280a7bb2972647f7851424497f9aed1c134d84
dylan <dylan.gabriele@vtsd.com>
2025-01-17 12:21:29 -0500

    project

--- 2-1-4/projec.py
+++ 2-1-4/projec.py
@@ -1,35 +1,48 @@
 #--Setup--
-#---Import Statements---
+#--Import Statements--
 import turtle
-import turtle as trtl
-import random as random
 
 #--Turtle Screen Initialization--
 screen = turtle.Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=550, height=550)
 
-#--Board--
-board = [["" for _ in range(3)] for _ in range(3)]
-player_1 = "X"
-tile_size = 250
+#--Variables--
+tic_tac_toe_board = [["" for _ in range(3)] for _ in range(3)]
+player_one = "X"
+panel_size = 200
 
-#--Drawing Board--
-def draw_board(board):
-    turtle_speed(10)
+#--TicTacToeBoard--
+def draw_board():
     turtle.penup()
-    turtle.goto(-300, 100)
-    turtle.pendown()
-    turtle.goto(300, 100)
-    turtle.penup()
-    turtle.goto(-300, -100)
-    turtle.pendown()
-    turtle.goto(300, -100)
-    turtle.penup()
-    turtle.goto(-100, 300)
-    turtle.pendown()
-    turtle.goto(-100, -300)
-    turtle.penup()
-    turtle.goto(100, 300)
-    turtle.pendown()
     turtle.goto(100, -300)
+    turtle.pendown()
+    turtle.goto(100, 300)
     turtle.penup()
+    turtle.goto(-100, -300)
+    turtle.pendown()
+    turtle.goto(-100, 300)
+    turtle.penup()
+    turtle.goto(300, -100)
+    turtle.pendown()
+    turtle.goto(-300, -100)
+    turtle.penup()
+    turtle.goto(300, 100)
+    turtle.pendown()
+    turtle.goto(-300, 100)
+    turtle.penup()
+
+
+#--Draw X--
+def draw_x(x, symbol):
+    turtle.penup()
+    turtle.goto(x, symbol)
+    turtle.write(symbol, align="center", font=("Arial", 24, "normal"))
+
+#--Click For Panels--
+def panel_from_click(x, y):
+
+
+#--Drawing the Board--
+draw_board()
+screen.mainloop()
+
