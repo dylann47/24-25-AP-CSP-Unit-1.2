6c2d69cac18c1f609a40d00ad33b7c525ebd5b69
dylan <dylan.gabriele@vtsd.com>
2025-02-18 12:11:37 -0500

    project

--- 2-1-4/projec.py
+++ 2-1-4/projec.py
@@ -1,4 +1,4 @@
-#--Setup--
+#--Setup Code--
 #--Import Statements--
 import turtle as turtle
 
@@ -31,18 +31,32 @@ def draw_board():
     turtle.goto(-300, 100)
     turtle.penup()
 
-
 #--Draw X--
-def draw_x(x, symbol):
+def draw_symbol(row, col, symbol):
+    x = -200 + (row * panel_size)
+    y = 200 - (row * panel_size)
+
     turtle.penup()
-    turtle.goto(x, symbol)
-    turtle.write(symbol, align="center", font=("Arial", 24, "normal"))
+    turtle.goto(x,y)
+    turtle.write(symbol, align="center", font=("Arial", 36, "normal"))
 
 #--Click For Panels--
+def click_on_panel(x,y):
+    col = (x + 300) // panel_size
+    row = (-y + 300) // panel_size
 
+    if 0 <= row < 3 and 0 <= col < 3 and tic_tac_toe_board[int(row)][int(col)] == "":
+        user_input = screen.textinput("Tic-Tac-Toe", "Enter X or O:").upper()  # Get user input
+        if user_input in ["X", "O"]:
+            tic_tac_toe_board[int(row)][int(col)] = user_input
+            draw_symbol(int(row), int(col), user_input)
+        else:
+            print("Invalid input! Please enter 'X' or 'O'.")
 
 
 #--Drawing the Board--
 draw_board()
+screen.onclick(click_on_panel)
 screen.mainloop()
+
 
