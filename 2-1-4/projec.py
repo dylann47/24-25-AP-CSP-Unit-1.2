50bbf90f056d81be8923c3d18cc5735778561137
dylan <dylan.gabriele@vtsd.com>
2025-02-19 11:22:39 -0500

    project

--- 2-1-4/projec.py
+++ 2-1-4/projec.py
@@ -4,15 +4,17 @@ import turtle as turtle
 
 #--Turtle Screen Initialization--
 screen = turtle.Screen()
-screen.setup(width=550, height=550)
+screen.setup(width=600, height=600)
 
 #--Variables--
 tic_tac_toe_board = [["" for _ in range(3)] for _ in range(3)]
 player_one = "X"
 panel_size = 200
+offset = 300
 
 #--TicTacToeBoard--
 def draw_board():
+    turtle.speed(0)
     turtle.penup()
     turtle.goto(100, -300)
     turtle.pendown()
@@ -33,7 +35,7 @@ def draw_board():
 
 #--Draw X--
 def draw_symbol(row, col, symbol):
-    x = -200 + (row * panel_size)
+    x = -200 + (col * panel_size)
     y = 200 - (row * panel_size)
 
     turtle.penup()
@@ -42,16 +44,16 @@ def draw_symbol(row, col, symbol):
 
 #--Click For Panels--
 def click_on_panel(x,y):
-    col = (x + 300) // panel_size
-    row = (-y + 300) // panel_size
+    col = int((x + offset) // panel_size)
+    row = int((offset - y) // panel_size)
 
-    if 0 <= row < 3 and 0 <= col < 3 and tic_tac_toe_board[int(row)][int(col)] == "":
-        user_input = screen.textinput("Tic-Tac-Toe", "Enter X or O:").upper()  # Get user input
+    if 0 <= row < 3 and 0 <= col < 3 and tic_tac_toe_board[row][col] =="":
+        user_input = screen.textinput("TicTacToe", "Choose Input").upper()
         if user_input in ["X", "O"]:
-            tic_tac_toe_board[int(row)][int(col)] = user_input
-            draw_symbol(int(row), int(col), user_input)
+            tic_tac_toe_board[row][col] = user_input
+            draw_symbol(row, col, user_input)
         else:
-            print("Invalid input! Please enter 'X' or 'O'.")
+            print("Not a valid choice")
 
 
 #--Drawing the Board--
